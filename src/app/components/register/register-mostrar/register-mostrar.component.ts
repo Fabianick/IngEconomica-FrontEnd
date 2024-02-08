cce767e7f854805e397083ffec9e09ac481cbee2
AnthonyNunez500 <u202119276@upc.edu.pe>
2024-02-08 09:48:47 -0500

    Versión inicial de la pantalla de register
    Declaración de los archivos de la pantalla de registro

---
+++ src/app/components/register/register-mostrar/register-mostrar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-mostrar',
+  templateUrl: './register-mostrar.component.html',
+  styleUrls: ['./register-mostrar.component.css']
+})
+export class RegisterMostrarComponent {
+
+}
