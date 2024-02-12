13bf5528b57c793f7548e941be8c1ea3aad15a16
AlexYucra2023 <145053618+AlexYucra2023@users.noreply.github.com>
2024-02-12 01:14:15 -0500

    Add files via upload

--- src/app/components/toolbar/toolbar-mostrar/toolbar-mostrar.component.ts
+++ src/app/components/toolbar/toolbar-mostrar/toolbar-mostrar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-toolbar-mostrar',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./toolbar-mostrar.component.css']
 })
 export class ToolbarMostrarComponent {
+  cssUrl4: string = '/assets/css/styles.css';
+  cssUrl5: string = '/assets/css/swiper-bundle.min.css';
 
+  constructor(
+    /*private _chargeScripts: ScriptChargeService,*/
+    public sanitizer: DomSanitizer/*,
+    private renderer: Renderer2,
+    private router: Router*/
+  ) { }
 }
